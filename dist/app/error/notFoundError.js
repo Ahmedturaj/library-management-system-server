cfc592798022c0d542d9eb76948895a2ba668a93
SK Toukir Ahmed Turaj <ahmedturaj12@gmail.com>
2025-08-31 14:35:13 +0600

    Delete dist directory

--- dist/app/error/notFoundError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundError = (req, res, next) => {
-    res
-        .status(404)
-        .json({ success: false, message: 'Not Found', path: req.path });
-};
-exports.default = notFoundError;
